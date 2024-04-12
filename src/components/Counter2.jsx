27e2c9d97efe8529e0cc295d826ad0888f24fdda
Qoopkite <qotodus@naver.com>
2024-04-12 10:53:35 +0900

    react

--- src/components/Counter2.jsx
+++ src/components/Counter2.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import {useState} from 'react'
+import React, {useEffect} from "react"
 
 const Counter2 = () => {
   // 이벤트 핸들러부터 state까지 모두 스스로 작성해보시길 바랍니다!
   // 과연 어떤 state를 생성해주어야하고, 또 어떤 이벤트 핸들러가 필요할까요?
   // useState와 useEffect를 둘다 사용해주셔서 진행해주시면 됩니다!
+  const [count,setcount]=useState(0);
+  const [text, setText]=useState('')
+
+  const handleText=event =>{
+    setText(event.target.value)
+  }
+
+  const increase=()=>{
+    setcount(count+1)
+  };
+
+  useEffect(()=>{
+    console.log("count값이 변경되었습니다")
+  },[count]);
+
+  useEffect(()=>{
+    console.log("랜더링이 완료되었습니다.")
+  },[]);
+
+  useEffect(()=>{
+    console.log("text값이 변경되었습니다")
+  },[text]);
 
   return (
     <div className="counter">
       <h>Counter 2</h>
       <hr></hr>
-      <h3>{/*카운트한 숫자가 들어가요*/}</h3>
-      <button>+1</button>
+      <h3>{count}</h3>
+      <button onClick={increase}>+1</button>
       <hr></hr>
-      <input type={'text'}></input>
-      <h3>{/*입력한 글자가 들어가요*/}</h3>
+      <input type={'text'} value={text} onChange={handleText}></input>
+      <h3>{text}</h3>
     </div>
   )
 }
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+
+
+
 
 const Name = () => {
+
+  const [name,setName]=useState('');
+  const [nickname, setNickname]=useState('')
+
+  const handleName=event=>{
+    setName(event.target.value)
+  }
+  
+  const handleNickname=event=>{
+    setNickname(event.target.value)
+
+  }
+
   return (
     <div className="name">
       <h>Name</h>
       <hr></hr>
-      <input name="myName" placeholder="이름을 입력해주세요" />
-      <input name="nickName" placeholder="닉네임을 입력해주세요" />
-      <p>이름</p>
-      <p>닉네임</p>
+      <input name="myName" placeholder="이름을 입력해주세요"
+       type={'name'} value={name} onChange={handleName}/>
+      <input name="nickName" placeholder="닉네임을 입력해주세요"
+      type={'nickname'} value={nickname} onChange={handleNickname} />
+      <p>이름:{name===''? '아직 이름이 없습니다':name}</p>
+      <p>닉네임:{nickname===''?'아직 닉네임이 없습니다':nickname}</p>
     </div>
   )
 }
